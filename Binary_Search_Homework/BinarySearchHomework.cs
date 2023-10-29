52ba60d8538fb2b63bbe05846f02f8d20d2d52f3
Dania-zoulghena <149172836+Dania-zoulghena@users.noreply.github.com>
2023-10-29 11:50:00 +0300

    Update BinarySearchHomework.cs

--- Binary_Search_Homework/BinarySearchHomework.cs
+++ Binary_Search_Homework/BinarySearchHomework.cs
@@ -14,6 +14,35 @@ namespace Binary_Search_Homework // don't edit this line!!!
                                                                                 // من الممكن تمرير قيمة 0 لتجاهلهم
         {
             //TODO: write code for the ternary search algorithm and return the index of the element
+            while (start <= end)
+            {
+                int mid1 = start + ((end - start) / 3);
+                int mid2 = end - ((end - start) / 3);
+
+                if (arr[mid1] == key)
+                {
+                    return mid1 + 1;
+                }
+
+                if (arr[mid2] == key)
+                {
+                    return mid2 + 1;
+                }
+
+                if (key < arr[mid1])
+                {
+                    end = mid1 - 1;
+                }
+                else if (key > arr[mid2])
+                {
+                    start = mid2 + 1;
+                }
+                else
+                {
+                    start = mid1 + 1;
+                    end = mid2 - 1;
+                }
+            }
             return -1;
         }
 
@@ -24,14 +53,68 @@ namespace Binary_Search_Homework // don't edit this line!!!
                                                                     // من الممكن تمرير قيمة 0 لتجاهلهم
         {
             //TODO: this methods is for getting the first accurence of the key and the last accurance
-            return -1;
+            int firstOccurrence = -1;
+
+            // البحث عن أول حدوث للعدد المستهدف
+            while (start <= end)
+            {
+                int mid = start + (end - start) / 2;
+
+                if (arr[mid] == key)
+                {
+                    firstOccurrence = mid;
+                    end = mid - 1; // البحث عن المزيد في الجزء الأيسر
+                }
+                else if (arr[mid] < key)
+                {
+                    start = mid + 1;
+                }
+                else
+                {
+                    end = mid - 1;
+                }
+            }
+
+            if (firstOccurrence == -1)
+            {
+                return 0; // لم يتم العثور على العدد المستهدف
+            }
+
+            // البحث عن آخر حدوث للعدد المستهدف
+            int lastOccurrence = firstOccurrence;
+            end = arr.Length - 1;
+
+            while (start <= end)
+            {
+                int mid = start + (end - start) / 2;
+
+                if (arr[mid] == key)
+                {
+                    lastOccurrence = mid;
+                    start = mid + 1; // البحث عن المزيد في الجزء الأيمن
+                }
+                else if (arr[mid] < key)
+                    start = mid + 1;
+
+                else
+                    end = mid - 1;
+            }
+
+            return lastOccurrence - firstOccurrence + 1; // عدد مرات التكرار            return -1;
         }
 
         public static int GetRepeatCount(int[] arr, int key) // don't edit this line!!!
         {
             //TODO: write code to calculate the repeat count of a spacific element
             // make sure to use the previous method in this method
-            return -1;
+        int count = 0;
+            for (int i = 0; i < arr.Length; i++)
+                if (arr[i] == key)
+                    count++;
+            return count;
+        }
+    }
+
         }
     }
 }
